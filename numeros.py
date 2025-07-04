434679419a7424f8b4521d75a1da6655f32efedd
Ricardobraz-17 <Ricardobrz754@gmail.com>
2025-07-04 14:48:30 +0000

    atividade

---
+++ numeros.py
@@ -0,0 +1,24 @@
+'''''print("numeros pares:")
+par = 2
+for i in range(20):
+    print(par)
+    par += 2
+
+
+print("numeros impar:")
+impar = 1
+for i in range(20):
+    print(impar)
+    impar += 2  '''''
+
+
+
+
+print("Sequência de Fibonacci:")
+a = 0
+b = 1
+for i in range(10):
+    print(a)
+    proximo = a + b
+    a = b
+    b = proximo
